b3dd8b3439c3d44c8cb10ff97c2f05454d46b2b2
RetoSchmucki <reto.schmucki@mail.mcgill.ca>
2018-01-04 18:10:56 +0000

    include github install

--- rbms_example.R
+++ rbms_example.R
@@ -1,8 +1,12 @@
 R --vanilla
 
+install.packages("devtools")
+library(devtools)
+install_github("RetoSchmucki/rbms", force=TRUE)
+
 library(rbms)
 
-help(flight_curve)
+?check_names
 
 data(m_visit)
 data(m_count)
@@ -19,24 +23,21 @@ ts_season_count <- ts_monit_count_site(ts_season_visit, m_count, sp=2)
 system.time(ts_flight_curve <- flight_curve(ts_season_count, NbrSample=100, MinVisit=3, MinOccur=2, MinNbrSite=1,
                             MaxTrial=3, FcMethod='regionalGAM', GamFamily='quasipoisson', SpeedGam=FALSE, CompltSeason=TRUE))
 
-system.time(ts_flight_curve <- flight_curve(ts_season_count,NbrSample=100,MinVisit=3,MinOccur=2,MinNbrSite=1,
-                            MaxTrial=3,FcMethod='regionalGAM',GamFamily='quasipoisson',CompltSeason=TRUE))
-
 ## plot the flight curves
 ts_flight_curve$f_pheno
 
 plot(ts_flight_curve$f_pheno[M_YEAR==2000,trimDAYNO],ts_flight_curve$f_pheno[M_YEAR==2000,NM],type='l',ylim=c(0,max(ts_flight_curve$f_pheno[,NM])),xlab='Monitoring Year Day',ylab='Relative Abundance')
 c <- 2
-for(y in 2001:2005){
+for(y in 2001:2003){
   points(ts_flight_curve$f_pheno[M_YEAR==y,trimDAYNO],ts_flight_curve$f_pheno[M_YEAR==y,NM],type='l',col=c)
   c <- c + 1
 }
-legend('topright',legend=c(2000:2005),col=c(seq_along(c(2000:2005))),lty=1,bty='n')
+legend('topright',legend=c(2000:2003),col=c(seq_along(c(2000:2003))),lty=1,bty='n')
 
 site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve)
 
 unique(site_year_sp_count$sp_ts_season_count[,SITE_ID])
-s <- 107
+s <- 2
 y <- 2003
 
 plot(site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y, FITTED],

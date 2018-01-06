292cdc5e1368912372b81a7a5f7a9bdbdaaa7ec2
RetoSchmucki <reto_schmucki@hotmail.com>
2018-01-06 20:35:32 +0000

    update example

--- minimal example.r
+++ minimal example.r
@@ -30,16 +30,20 @@ ts_season_count <- ts_monit_count_site(ts_season_visit, m_count, sp=2)
 ##=========================================
 
 system.time(ts_flight_curve <- flight_curve(ts_season_count, NbrSample=100, MinVisit=3, MinOccur=2, MinNbrSite=1,
-                            MaxTrial=3, FcMethod='regionalGAM', GamFamily='quasipoisson', SpeedGam=FALSE, CompltSeason=TRUE))
+                            MaxTrial=3, FcMethod='regionalGAM', GamFamily='quasipoisson', SpeedGam=FALSE, 
+                            CompltSeason=TRUE, KeepModel = TRUE))
 
+ts_flight_curve[1]
+names(flight_curve_model)
 ## 4.
 ## plot the flight curves 
 ##=========================================
 
-plot(ts_flight_curve$f_pheno[M_YEAR==2000,trimDAYNO],ts_flight_curve$f_pheno[M_YEAR==2000,NM],type='l',ylim=c(0,max(ts_flight_curve$f_pheno[,NM])),xlab='Monitoring Year Day',ylab='Relative Abundance')
+plot(ts_flight_curve[M_YEAR==2000, trimDAYNO], ts_flight_curve[M_YEAR==2000, NM], type='l', 
+      ylim=c(0, max(ts_flight_curve[, NM])), xlab='Monitoring Year Day', ylab='Relative Abundance')
 c <- 2
 for(y in 2001:2003){
-  points(ts_flight_curve$f_pheno[M_YEAR==y,trimDAYNO],ts_flight_curve$f_pheno[M_YEAR==y,NM],type='l',col=c)
+  points(ts_flight_curve[M_YEAR==y, trimDAYNO], ts_flight_curve[M_YEAR==y, NM], type='l', col=c)
   c <- c + 1
 }
 legend('topright',legend=c(2000:2003),col=c(seq_along(c(2000:2003))),lty=1,bty='n')
@@ -51,18 +55,22 @@ legend('topright',legend=c(2000:2003),col=c(seq_along(c(2000:2003))),lty=1,bty='
 ## regionalGAM method
 ##=========================================
 
-site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve, SpeedGlm = FALSE, FamilyGlm = 'poisson')
+site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve, SpeedGlm = FALSE, FamilyGlm = 'quasipoisson',
+                                  KeepModel = TRUE)
 
+names(imp_glm_model)
 ## 6.
 ## plot the imputed and observed counts for one
 ## site (e.g. 2) and one year (e.g 2003)
 ##=========================================
 
-s <- 2
+s <- 4
 y <- 2003
 
-plot(site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y, FITTED],
-    ylim=c(0,max(site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y,COUNT_IMPUTED])), col='blue', type='l', main=paste0('Site ', s, ', Season ', y), 
-    xlab='Monitoring Month', ylab='Fitted Count')
-points(site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count$sp_ts_season_count[SITE_ID == s & M_YEAR == y, COUNT],
-       col='red')
+plot(site_year_sp_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count[SITE_ID == s & M_YEAR == y, FITTED],
+    ylim = c(0,max(site_year_sp_count[SITE_ID == s & M_YEAR == y,COUNT_IMPUTED])), 
+    col ='blue', type='l', 
+    main = paste0('Site ', s, ', Season ', y), 
+    xlab = 'Monitoring Month', ylab='Fitted Count')
+points(site_year_sp_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count[SITE_ID == s & M_YEAR == y, COUNT],
+       col = 'red')

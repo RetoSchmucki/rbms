10ef801ef654e5aa499a17a3491a0e3a015541e1
RetoSchmucki <reto.schmucki@mail.mcgill.ca>
2018-01-08 17:09:53 +0000

    update rbms example

--- rbms_example.R
+++ rbms_example.R
@@ -4,9 +4,18 @@ install.packages("devtools")
 devtools::install_github("RetoSchmucki/rbms", force=TRUE)
 library(rbms)
 
+
+## 1.
+## load data included in the package
+##==================================
 data(m_visit)
 data(m_count)
 
+
+## 2.
+## organize the data to cover the time 
+## period and monitoring season of the BMS
+##=========================================
 ts_date <- ts_dwmy_table(InitYear = 2000, LastYear = 2003, WeekDay1 = 'monday')
 
 ts_season <- ts_monit_season(ts_date, StartMonth = 4, EndMonth = 9, StartDay = 1, EndDay = NULL,
@@ -16,12 +25,18 @@ ts_season_visit <- ts_monit_site(m_visit, ts_season)
 
 ts_season_count <- ts_monit_count_site(ts_season_visit, m_count, sp = 2)
 
-system.time(ts_flight_curve <- flight_curve(ts_season_count, NbrSample = 200, MinVisit = 4, MinOccur = 3, MinNbrSite = 1,
+
+## 3.
+## compute the flight curve, using the
+## regionalGAM method
+##=========================================
+system.time(ts_flight_curve <- flight_curve(ts_season_count, NbrSample = 200, MinVisit = 5, MinOccur = 3, MinNbrSite = 1,
                             MaxTrial = 3, FcMethod = 'regionalGAM', GamFamily = 'quasipoisson', SpeedGam = FALSE, CompltSeason = TRUE))
 
-site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve, SpeedGlm = FALSE, FamilyGlm = 'quasipoisson')
 
-## plot the flight curves
+## 4.
+## plot the flight curves 
+##=========================================
 ts_flight_curve
 
 plot(ts_flight_curve[M_YEAR == 2000, trimDAYNO], ts_flight_curve[M_YEAR == 2000, NM], type = 'l', 
@@ -33,8 +48,27 @@ for(y in 2001:2003){
 }
 legend('topright', legend = c(2000:2003), col = c(seq_along(c(2000:2003))), lty = 1, bty = 'n')
 
-unique(site_year_sp_count[, SITE_ID])
 
+## 5.
+## retrieve GAM models
+##=========================================
+names(flight_curve_model)
+summary(flight_curve_model$FlightModel_2_2003)
+
+
+## 6.
+## impute the count for the missing day, using
+## the flight curve computed with the 
+## regionalGAM method
+##=========================================
+site_year_sp_count <- impute_count(ts_season_count, ts_flight_curve, SpeedGlm = FALSE, FamilyGlm = 'quasipoisson')
+
+
+## 7.
+## plot the imputed and observed counts for one
+## site (e.g. 2) and one year (e.g 2003)
+##==============================================
+unique(site_year_sp_count[, SITE_ID])
 s <- 2
 y <- 2003
 
@@ -46,12 +80,10 @@ plot(site_year_sp_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count[SI
 points(site_year_sp_count[SITE_ID == s & M_YEAR == y, DATE], site_year_sp_count[SITE_ID == s & M_YEAR == y, COUNT],
        col='red')
 
-site_year_sp_count
 
+site_year_sp_count
+## 8.
 ## retrieve GLM models 
+##==============================================
 names(impute_glm_model)
 summary(impute_glm_model$glm_mod_2_2003)
-
-## retrieve GAM models
-names(flight_curve_model)
-summary(flight_curve_model$FlightModel_2_2003)

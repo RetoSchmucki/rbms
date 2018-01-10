4c6c07c0894fa1faaf7ed4a751bb4e9523564d64
RetoSchmucki <reto.schmucki@mail.mcgill.ca>
2018-01-10 12:33:01 +0000

    fix plot get_raster_value

--- R/rbms_toolbox.r
+++ R/rbms_toolbox.r
@@ -28,8 +28,8 @@
 check_package <- function(pkgName=NULL, message1='you need to install the package ',message2='This version requires '){
         if (!requireNamespace(pkgName)) {
             print(paste(message1, pkgName))
-            x <- readline(paste("Do you want to install",pkgName,"? Y/N"))            
-            if (toupper(x) == 'Y') { 
+            x <- readline(paste("Do you want to install",pkgName,"? Y/N"))
+            if (toupper(x) == 'Y') {
                     install.packages(pkgName)
             }
             if (toupper(x) == 'N') {
@@ -43,9 +43,9 @@ check_package <- function(pkgName=NULL, message1='you need to install the packag
 #' Verify for the required column names in the data
 #' @param x a data.table object with column names
 #' @param y vector with the required variable names
-#' @return Verify if column names listed in \code{y} vector are found in the data set \code{x}, if not, a message identifies the 
+#' @return Verify if column names listed in \code{y} vector are found in the data set \code{x}, if not, a message identifies the
 #' missing column name and stops.
-#' @details This function is not case sensitive, but it does not accept different names or spelling. 
+#' @details This function is not case sensitive, but it does not accept different names or spelling.
 #' @author Reto Schmucki - \email{reto.schmucki@@mail.mcgill.ca}
 #' @examples
 #' DF <- data.frame(DAY=c(1:5),MONTH=rep(month.name[2],5),YEAR=rep(format(Sys.Date(),'%Y'),5))
@@ -65,7 +65,7 @@ check_names <- function(x, y){
 #' ts_date_seq
 #' Generate a time-series of dates (per day) from the beginning of a starting year to the end of an ending years.
 #' @param InitYear start year of the time-series, 4 numbers format (e.g 1987)
-#' @param LastYear end year of the time-series, if not provided, current year is used instead 
+#' @param LastYear end year of the time-series, if not provided, current year is used instead
 #' @return return a POSIXct vector with the format 'YYYY-MM-DD HH:MM:SS'
 #' @keywords time series
 #' @author Reto Schmucki - \email{reto.schmucki@@mail.mcgill.ca}
@@ -85,9 +85,9 @@ ts_date_seq <- function(InitYear=1970,LastYear=format(Sys.Date(),"%Y")) {
     }
 
 
-#' get_raster_value 
+#' get_raster_value
 #' Extract raster value for a set of geographic points
-#' @param x data frame with longitude and latitude in column 1 and 2 respectively 
+#' @param x data frame with longitude and latitude in column 1 and 2 respectively
 #' @param yPath complete path to raster layer from which value should be extracted
 #' @param Classification a data.frame with classification for the raster values
 #' @param xCrs EPSG number of the cordinate reference system (CRS) of the x coordinates, see \code{\link{http://spatialreference.org/}}
@@ -96,11 +96,11 @@ ts_date_seq <- function(InitYear=1970,LastYear=format(Sys.Date(),"%Y")) {
 #' @param PlotRaster logical informing if points, their bounding box and the raster layer should be plotted
 #' @return return a df or a sf object with value extracted from the y layer, see \code{out_df}.
 #' @author Reto Schmucki - \email{reto.schmucki@@mail.mcgill.ca}
-#' @export get_raster_value 
-#' @examples 
+#' @export get_raster_value
+#' @examples
 #' x <- data.frame(longitude = c(4, 4.1, 4.5), latitude = c(50, 50.45, 50.5), id = c('a','b','c'))
 #' (x_value <- get_raster_value(x))
-#'  (x_value <- get_raster_value(x, PlotRaster = TRUE))
+#' (x_value <- get_raster_value(x, PlotRaster = TRUE))
 #'
 
 get_raster_value <- function(x, yPath = 'metzger_v3_europe' , Classification = NULL, xCrs = 4326, BufferDist = NULL, OutDf = TRUE, PlotRaster = FALSE){
@@ -124,16 +124,17 @@ get_raster_value <- function(x, yPath = 'metzger_v3_europe' , Classification = N
         }
         focal_box <- as(sf::st_buffer(sf::st_as_sfc(sf::st_bbox(x_sf)), raster::res(y_r)[1]*50), "Spatial")
         layer_cr <- raster::crop(y_r, raster::extent(focal_box))
-        
+
         if(isTRUE(PlotRaster)){
+            dev.new()
             raster::plot(y_r)
-            plot(focal_box, col = 'orange', lwd = 1, lty = 2, add = TRUE)
+            plot(sf::st_as_sf(focal_box)$geometry, col = 'orange', lwd = 1, lty = 2, add = TRUE)
             plot(x_sf$geometry, col = 'blue', pch = 19, add = TRUE)
         }
-        
+
         x_sf$value <- raster::extract(layer_cr,as(x_sf,"Spatial"))
         x_sf <- sf::st_transform(x_sf, xCrs)
-        
+
         if(isTRUE(OutDf)){
             x_sf$longitude <- sf::st_coordinates(x_sf)[,1]
             x_sf$latitude <- sf::st_coordinates(x_sf)[,2]
